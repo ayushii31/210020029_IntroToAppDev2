c07e55a84acf68c42c531a1172ecf9a6d4a9f0eb
ayushii31 <133577405+ayushii31@users.noreply.github.com>
2023-07-21 01:42:16 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\anany\\Downloads\\flutter_windows_3.10.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\anany\\OneDrive\\Desktop\\flutter projects\\classico" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\anany\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\anany\\OneDrive\\Desktop\\flutter projects\\classico"
+  "FLUTTER_ROOT=C:\\Users\\anany\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\anany\\OneDrive\\Desktop\\flutter projects\\classico\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\anany\\OneDrive\\Desktop\\flutter projects\\classico"
+  "FLUTTER_TARGET=C:\\Users\\anany\\OneDrive\\Desktop\\flutter projects\\classico\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80NWY2ZTAwOTExMGRmNGYzNGVjMmNmOTlmNjNjZjczYjcxYjdhNDIwLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\anany\\OneDrive\\Desktop\\flutter projects\\classico\\.dart_tool\\package_config.json"
+)
